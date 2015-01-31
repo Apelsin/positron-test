523b5e1c19b5af720a1e636d1eb8f1d22aff2129
Apelsin <appelsindrikken@gmail.con>
2015-01-30 17:32:32 -0800

    Beginnings

---
+++ Punky Town/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Positron;
+
+namespace PunkyTown
+{
+    static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            // Instantiate the main window
+            // this also sets up OpenGL
+            var main_window = new ThreadedRendering();
+            // Prepare game resources
+            // This makes OpenGL calls
+            PositronGame.InitialSetup();
+            // TEST: Dump all the settings:
+            //Configuration.DumpEverything ();
+            // Run the window thread
+            // Game will be set up by render/update thread -because reasons-
+            main_window.Run();
+        }
+    }
+}
